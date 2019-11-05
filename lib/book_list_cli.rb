d55c0611cc742f952a48ae60a38e0d75b79f427b
Bryce Eadie <bryce.eadie@gmail.com>
2019-11-05 01:34:01 -0800

    Initial commit

---
+++ lib/book_list_cli.rb
@@ -0,0 +1,6 @@
+require "book_list_cli/version"
+
+module BookListCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/book_list_cli/version.rb
@@ -0,0 +1,3 @@
+module BookListCli
+  VERSION = "0.1.0"
+end
