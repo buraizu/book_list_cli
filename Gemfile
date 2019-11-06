fbe53eb7e00a2fc4451e56ae4b0e9c51c333aeb2
Bryce Eadie <bryce.eadie@gmail.com>
2019-11-06 01:23:25 -0800

    Implement GoogleBooks API call

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in book_list_cli.gemspec
 gemspec
+
+gem "googlebooks", "~> 0.0.9"
--- lib/book_list_cli/cli.rb
+++ lib/book_list_cli/cli.rb
@@ -1,6 +1,26 @@
 # CLI Controller
+require 'googlebooks'
+
 class BookListCli::CLI
     def call
-        puts "Check out a booklist"
+        menu
+        goodbye
     end
+
+    def menu
+        input = nil
+        while input != "exit"
+            puts "Enter your search query"
+            input = gets.strip
+            books = GoogleBooks.search(input)
+            first_book = books.first
+            puts first_book.authors
+            puts first_book.title
+            puts first_book.publisher
+        end
+    end
+
+    def goodbye
+        puts "See you tomorrow for more books"
+    end 
 end
